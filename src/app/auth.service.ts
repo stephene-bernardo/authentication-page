003c2b45dc32199888c10e5265a828b2fd65c561
lrakarl <stephene.bernardo@gmail.com>
2018-10-14 19:19:21 +0800

    refactor auth service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -4,18 +4,36 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import {
+  AuthService as SocialAuthService,
+  FacebookLoginProvider,
+  GoogleLoginProvider
+} from 'angular5-social-login';
 
+declare const gapi: any;
 @Injectable()
 export class AuthService {
   baseUrl = 'http://localhost:3000';
 
-  constructor(private http: HttpClient, private router: Router) { }
+  GoogleAuth;
+  public auth2: any;
+  constructor(private http: HttpClient, private router: Router, private authService: SocialAuthService) { 
+    this.googleInit();
+  }
 
   loginUsingDbAuth(login: string, password: string): Observable<any> {
     const httpOptions = {
       responseType: 'text' as 'text'
     };
     return this.http.post(`${this.baseUrl}/auth`, { username: login, password: password }, httpOptions);
+  }
+
+  loginUsingFacebookAuth() {
+    return this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  loginUsingGoogleAuth() {
+    return this.GoogleAuth.signIn().then(() => this.GoogleAuth.currentUser.get()['Zi']['id_token']);
   }
 
   getUserInfo(authToken: string): Observable<any>  {
@@ -38,4 +56,16 @@ export class AuthService {
     }
   }
 
+
+  public googleInit() {
+    gapi.load('auth2', () => {
+      gapi.auth2.init({
+        client_id: '82798158560-b44domiskkgha8gm1l95c951f1r9ulvc.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      }).then(res => {
+        this.GoogleAuth = gapi.auth2.getAuthInstance();
+      });
+    });
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,84 +1,46 @@
 import { ConstantsClass } from './../constants';
-import { AuthService as InternalAuthService } from './../auth.service';
-import { Component, OnInit, AfterViewInit, NgZone } from '@angular/core';
-
-
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { sign } from 'jsonwebtoken';
+import { AuthService} from './../auth.service';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-declare const gapi: any;
-import {
-  AuthService,
-  FacebookLoginProvider,
-  GoogleLoginProvider
-} from 'angular5-social-login';
-import { log } from 'util';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements AfterViewInit {
+export class LoginComponent {
 
-  GoogleAuth;
-  public auth2: any;
   authIdentifier = 'authToken';
-  constructor(private http: HttpClient, private router: Router, 
-    private zone: NgZone, private authService: AuthService, private internalAuthService: InternalAuthService) {
-    this.loadScript();
-  }
-
-  public loadScript() {
-    const url = 'https://apis.google.com/js/platform.js';
-    console.log('preparing to load...');
-    const node = document.createElement('script');
-    node.src = url;
-    node.type = 'text/javascript';
-    node.async = true;
-    node.charset = 'utf-8';
-    document.getElementsByTagName('head')[0].appendChild(node);
+  constructor(private http: HttpClient,
+    private router: Router,
+    private authService: AuthService) {
   }
 
   onSignInDbAuth(login, password) {
-    this.internalAuthService.loginUsingDbAuth(login, password).subscribe((res) => {
-      if (res !== 'Not Authenticated User') {
-        localStorage.setItem(this.authIdentifier, `${ConstantsClass.DATABASE_AUTH_IDENTIFIER} ${res}`);
-        this.router.navigate(['']);
+    this.authService.loginUsingDbAuth(login, password).subscribe(token => {
+      if (token !== 'Not Authenticated User') {
+        this.storeAuthTokenAndRedirect(ConstantsClass.DATABASE_AUTH_IDENTIFIER, token);
       }
     });
   }
 
-  public googleInit() {
-    gapi.load('auth2', () => {
-      gapi.auth2.init({
-        client_id: '82798158560-b44domiskkgha8gm1l95c951f1r9ulvc.apps.googleusercontent.com',
-        cookiepolicy: 'single_host_origin',
-        scope: 'profile email'
-      }).then(res => {
-        this.GoogleAuth = gapi.auth2.getAuthInstance();
-      });
-    });
-  }
-
   onSignInFacebookAuth() {
-    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then(
-      (userData) => {
-        localStorage.setItem(this.authIdentifier, `${ConstantsClass.FACEBOOK_AUTH_IDENTIFIER} ${userData.token}`);
-        this.router.navigate(['']);
+    this.authService.loginUsingFacebookAuth().then(userData => {
+        this.storeAuthTokenAndRedirect(ConstantsClass.FACEBOOK_AUTH_IDENTIFIER,
+          userData.token);
       }
     );
   }
 
   public onSignInGoogleAuth() {
-    this.GoogleAuth.signIn().then(() => {
-      const authToken = this.GoogleAuth.currentUser.get()['Zi']['id_token'];
-      localStorage.setItem(this.authIdentifier, `${ConstantsClass.GOOGLE_AUTH_IDENTIFIER} ${authToken}`);
-      this.zone.run(() => this.router.navigate(['']));
+    this.authService.loginUsingGoogleAuth().then((authToken) => {
+      this.storeAuthTokenAndRedirect(ConstantsClass.GOOGLE_AUTH_IDENTIFIER, authToken);
     });
   }
 
-  ngAfterViewInit() {
-    this.googleInit();
+  storeAuthTokenAndRedirect(authMethod, authToken) {
+    localStorage.setItem(this.authIdentifier, `${authMethod} ${authToken}`);
+    this.router.navigate(['']);
   }
 }
